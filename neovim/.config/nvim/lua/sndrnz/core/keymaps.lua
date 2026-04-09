680300568faaa7d83ba382d486e6b734d9959c29
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2026-04-09 09:58:01 +0200

    feat(neovim): update window split keymaps to match defaults

--- neovim/.config/nvim/lua/sndrnz/core/keymaps.lua
+++ neovim/.config/nvim/lua/sndrnz/core/keymaps.lua
@@ -8,11 +8,6 @@ vim.keymap.set("n", "gp", "`[v`]")
 
 vim.keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
 
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
-
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
