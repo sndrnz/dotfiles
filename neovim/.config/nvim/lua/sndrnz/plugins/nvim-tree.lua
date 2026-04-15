a2b5b7a546dc602fbc02b9fd376ff87c433c30f2
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2026-04-15 11:22:58 +0200

    fix(neovim): only filter '.git' folder

--- neovim/.config/nvim/lua/sndrnz/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/sndrnz/plugins/nvim-tree.lua
@@ -41,7 +41,7 @@ return {
         },
       },
       filters = {
-        custom = { ".DS_Store", ".git", "__pycache__" },
+        custom = { ".DS_Store", "^.git$", "__pycache__" },
       },
       git = {
         ignore = false,
